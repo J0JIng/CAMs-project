66d2592ab7852088389ec1025d799bb696f982fb
kyew003 <130239490+kyew003@users.noreply.github.com>
2023-11-05 00:35:06 +0800

    Update Enquiry.java

--- src/models/Enquiry.java
+++ src/models/Enquiry.java
@@ -1,5 +1,81 @@
 package models;
 
+import enums.MessageStatus;
+
 public class Enquiry {
+    private int enquiryID;
+    private String campName; 
+    private String senderID;
+    private String responderID;
+    private MessageStatus enquiryStatus;
+    private String enquiryMessage;
+    private String enquiryResponse;
+
+    public Enquiry(int enquiryID, String senderID, String campName, String message){
+
+        this.enquiryID = enquiryID;
+        this.senderID = senderID;
+        this.enquiryMessage = message;
+        this.enquiryStatus = MessageStatus.DRAFT;
+        this.enquiryResponse = null; //no response yet 
+        this.responderID = null; //no response yet 
+
+        public int getEnquiryID() {
+            return enquiryID;
+        }
+    
+        public void setEnquiryID(int enquiryID) {
+            this.enquiryID = enquiryID;
+        }
+    
+        public String getSenderID() {
+            return senderID;
+        }
+    
+        public void setSenderID(String senderID) {
+            this.senderID = senderID;
+        }
+        public String getcampName() {
+            return campName;
+        }
+    
+        public void setcampName(String campName) {
+            this.campName= campName;
+        }
+    
+        public String getResponderID() {
+            return responderID;
+        }
+    
+        public void setResponderID(String responderID) {
+            this.responderID = responderID;
+        }
+    
+        public MessageStatus getEnquiryStatus() {
+            return enquiryStatus;
+        }
+    
+        public void setEnquiryStatus(MessageStatus enquiryStatus) {
+            this.enquiryStatus = enquiryStatus;
+        }
+    
+        public String getEnquiryMessage() {
+            return enquiryMessage;
+        }
+    
+        public void setEnquiryMessage(String enquiryMessage) {
+            this.enquiryMessage = enquiryMessage;
+        }
+    
+        public String getEnquiryResponse() {
+            return enquiryResponse;
+        }
+    
+        public void setEnquiryResponse(String enquiryResponse) {
+            this.enquiryResponse = enquiryResponse;
+        }
+
+
+    }
     
 }
