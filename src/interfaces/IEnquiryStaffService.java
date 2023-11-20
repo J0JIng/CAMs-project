b5591b15ffd3b51e1f488eff7f981e933479e157
Jing <ojing12321@gmail.com>
2023-11-21 02:03:02 +0800

    Update IEnquiryStaffService.java

--- src/interfaces/IEnquiryStaffService.java
+++ src/interfaces/IEnquiryStaffService.java
@@ -1,35 +1,5 @@
 package interfaces;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.stream.Collectors;
-import models.Enquiry;
-import models.Enquiry;
-import stores.DataStore;
-import enums.MessageStatus;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
 public interface IEnquiryStaffService {
-
-    /**
-     * Retrieves a list of all enquiries related to camps.
-     * 
-     * This method returns a list containing all the enquiries submitted by students
-     * for various camps. Each element in the list represents an {@link Enquiry} object.
-     * The list may be empty if there are no enquiries recorded.
-     * 
-     * @return a {@link List} of {@link Enquiry} objects representing all camp enquiries
-     *
-     */
-    //public ArrayList<Enquiry> getAllPendingEnquiry();
-
-    //public ArrayList<Enquiry> getStaffRespondedEnquiry();
-
-
-
+    
 }
